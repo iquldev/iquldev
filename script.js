00d6e81648885db8325bcafca152e9787e331281
iquldev <141398177+iquldev@users.noreply.github.com>
2024-06-26 12:59:42 +0300

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-const textToCopy = 'play.iquldev.pro';
-function copyText() {
-    navigator.clipboard.writeText(textToCopy).then(() => {
-        console.log('Текст успешно скопирован!');
-    }, (error) => {
-        console.error('Ошибка при копировании текста:', error);
-    });
-    alert("Copied!");
-}
